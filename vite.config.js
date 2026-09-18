723e28b0fd1bb8a1062e1b26af99710f425f5e73
HckR01 <139985239+HckR01@users.noreply.github.com>
2026-09-18 21:58:30 +0530

    Pwa setup Done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,45 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "QuickChat",
+        short_name: "QuickChat",
+
+        description: "QuickChat Progressive Web App",
+
+        start_url: "/",
+        display: "standalone",
+
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   server: {
-    host: true, // Exposes on local network for mobile & multi-device testing
+    host: true,
     port: 5173,
   },
 });
